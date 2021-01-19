1d146ec4efdb8155b1394085867be9f0a375df48
Jin-devST <31203142+Jin-dev92@users.noreply.github.com>
2021-01-19 16:31:20 +0900

    어드민페이지리스트확인 TDD 추가

--- src/test/java/com/devjin/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/devjin/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,10 +16,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
     @After
     public void destoryAll(){
         postsRepository.deleteAll();
@@ -45,21 +49,34 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContents()).isEqualTo(contents);
     }
-//    @Test
-//    public void 작성일수정일되는지(){
-//        //given
-//        postsRepository.save(Posts.builder()
-//                .title("title")
-//                .contents("content")
-//                .author("김의진")
-//                .build());
-//        //when
-//        List<Posts> dataList = postsRepository.findAll();
-//        Posts firstObj  = dataList.get(0);
-////        LocalDateTime now = LocalDateTime.of(2021,1,13,0,0,0);
-////        System.out.println(now + "ㅇㄹㄴㅇㄹㄴㅇㄹㅇㄴㄹㄴㅇㄹ");
-//        //then
-//        assertThat(firstObj.getCreateDate()).isAfter(LocalDateTime.now());
-//        assertThat(firstObj.getModifiedDate()).isAfter(LocalDateTime.now());
-//    }
+    @Test
+    public void 작성일수정일되는지(){
+        //given
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .contents("content")
+                .author("김의진")
+                .build());
+        //when
+        List<Posts> dataList = postsRepository.findAll();
+        Posts firstObj  = dataList.get(0);
+        //then
+        assertThat(firstObj.getCreateDate()).isAfter(LocalDateTime.now());
+        assertThat(firstObj.getModifiedDate()).isAfter(LocalDateTime.now());
+    }
+
+    @Test
+    public void 어드민페이지리스트확인(){
+        //given 더미 데이터 입력
+        postsRepository.save(Posts.builder()
+                .title("제제제제제제제목")
+                .contents("내용내냉내용")
+                .author("김의진")
+                .build());
+        //when 비교할 데이터 가공
+        String body = testRestTemplate.getForObject("/api/admin",String.class);
+        //then 리스트가 예상했던 것과 같게 나와야함
+        assertThat(body).contains("김의진");
+    }
+
 }
