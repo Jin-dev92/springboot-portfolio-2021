3d87b86f6a866ff4077d2831dd094e11e52b643d
Jin-devST <31203142+Jin-dev92@users.noreply.github.com>
2021-02-04 13:35:42 +0900

    admin page using react library

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,4 @@
-import './App.css';
-import React from 'react';
-import { HashRouter, Route, Router } from 'react-router-dom';
-
-import Navigation from './component/Navigation';
-import Home from './routes/Home';
-import Admin from './routes/Admin';
-import Posts from './routes/Posts';
-import Kokoa from './routes/Kokoa';
 function App() {
-  return (
-    <HashRouter>
-      <Navigation component={Navigation}/>
-      <Route path="/" exact={true} component={Home}/>
-      <Route path="/admin" component={Admin}/>
-      <Route path="/posts" component={Posts}/>
-      <Route path="/kokoa" component={Kokoa}/>
-      {/* <Route path="/movie/:id" component={Detail}/> */}
-    </HashRouter>
-  );
+    return 'app';
 }
-
 export default App;
---
+++ src/main/java/com/devjin/web/IndexController.java
@@ -0,0 +1,2 @@
+package com.devjin.web;public class IndexController {
+}
